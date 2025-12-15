0e18579c5b29831d95b1d29865305e5516000ebd
janacosmo2020-bipe <janacosmo2020@gmail.com>
2025-12-14 21:54:41 -0300

    feat: adiciona lista de tarefas em memória

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return jsonify({"mensagem": "API de Tarefas Colaborativas"})
 
+tarefas = [
+    {
+        "id": 1,
+        "titulo": "Reunião de planejamento",
+        "descricao": "Organizar o cronograma do projeto.",
+        "colaboradores": ["João", "Maria"]
+    }
+]
+
 if __name__ == "__main__":
     app.run(debug=True)
